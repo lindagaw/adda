3adfa4b82223924476d6195e11408958579bd146
Ashley Gao <gaoye0718@gmail.com>
2022-08-12 01:16:26 -0400

    WIP

--- main.py
+++ main.py
@@ -45,16 +45,13 @@ if __name__ == '__main__':
     print(">>> Source Classifier <<<")
     print(src_classifier)
 
-'''
-    if not (src_encoder.restored and src_classifier.restored and
-            params.src_model_trained):
-        src_encoder, src_classifier = train_src(
-            src_encoder, src_classifier, src_data_loader)
+    src_encoder, src_classifier = train_src(src_encoder, src_classifier, src_data_loader)
 
     # eval source model
     print("=== Evaluating classifier for source domain ===")
     eval_src(src_encoder, src_classifier, src_data_loader_eval)
 
+'''
     # train target encoder by GAN
     print("=== Training encoder for target domain ===")
     print(">>> Target Encoder <<<")
